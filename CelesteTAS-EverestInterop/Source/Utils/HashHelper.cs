a7924838de39fcb6bf65353f033f1ceff65e28af
psyGamer <psygamer21@gmail.com>
2024-09-03 10:49:03 +0200

    Don't use Everest.ChecksumHasher

--- CelesteTAS-EverestInterop/Source/Utils/HashHelper.cs
+++ CelesteTAS-EverestInterop/Source/Utils/HashHelper.cs
@@ -1,10 +1,19 @@
+using System;
+using System.Reflection;
+using System.Security.Cryptography;
 using System.Text;
 using Celeste.Mod;
 
 namespace TAS.Utils;
 
 internal static class HashHelper {
+    private static MethodInfo m_Create;
+    
     public static string ComputeHash(string text) {
-        return Everest.ChecksumHasher.ComputeHash(Encoding.UTF8.GetBytes(text)).ToHexadecimalString();
+        m_Create ??= typeof(Everest).Assembly.GetType("Celeste.Mod.XXHash64")?.GetMethodInfo("Create", new Type[]{}, BindingFlags.Public | BindingFlags.Static); 
+        m_Create ??= typeof(Everest).Assembly.GetType("YYProject.XXHash.XXHash64")?.GetMethodInfo("Create", new Type[]{}, BindingFlags.Public | BindingFlags.Static);
+        
+        HashAlgorithm algo = (HashAlgorithm)m_Create!.Invoke(null, new object[]{});
+        return algo.ComputeHash(Encoding.UTF8.GetBytes(text)).ToHexadecimalString();
     }
 }
